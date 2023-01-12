f8c1582f06ef8f587530bfe9a65b6f769cf297a5
SugarSBN <15952265117@126.com>
2023-01-12 16:56:44 +0800

    Site updated: 2023-01-12 16:56:38

--- computer-graphics/fragment_shader.glsl
+++ computer-graphics/fragment_shader.glsl
@@ -1,9 +1,11 @@
 #version 300 es
 
-precision highp float;
+precision mediump float;
 
+in vec4 vColor;
 out vec4 fColor;
 
-void main(){
-    fColor = vec4(0.0, 0.0, 0.0, 1.0);
+void main()
+{
+    fColor = vColor;
 }
